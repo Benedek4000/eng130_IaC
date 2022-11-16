5011264ae196ed140d1e3e0ba67e5108c6eb7616
Benedek4000 <kovacsbenedek4000@gmail.com>
2022-11-16 09:40:20 +0000

    cahnges playbooks

--- VM/provision_controller.sh
+++ VM/provision_controller.sh
@@ -7,3 +7,4 @@ sudo apt install ansible -y
 sudo apt install sshpass -y
 sudo apt install tree -y
 sudo rm /etc/ansible/hosts && sudo cp sync_controller/hosts /etc/ansible/hosts
+sudo cp sync_controller/main.yml /etc/ansible/main.yml
